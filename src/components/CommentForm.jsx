aa4938177e41f1720a9a8e0bfe44aaca29a37a1d
gordon <gordon.song@thoughtworks.com>
2019-03-10 17:39:14 +0800

    add a new test case when the user fills in their comment

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 
 export default class CommentForm extends Component {
+  state = {
+    comment: "",
+    author: ""
+  };
+
+  handleOnChange = ({ target: { name, value } }) =>
+    this.setState(_prevState => ({
+      [name]: value
+    }));
+
+  hasInvalidFields = () => {
+    const { comment, author } = this.state;
+    if (comment.trim() !== "" && author.trim() !== "") {
+      return false;
+    }
+    return true;
+  };
+
   render() {
+    const { comment, author } = this.state;
+    const isDisabled = this.hasInvalidFields() ? true : null;
     return (
       <form data-testid="comment-form">
         <div>
-          <textarea placeholder="Write something..." name="comment" />
+          <textarea
+            placeholder="Write something..."
+            name="comment"
+            value={comment}
+            onChange={this.handleOnChange}
+          />
         </div>
         <div>
           <label htmlFor="author" aria-labelledby="author">
             Your Name
           </label>
-          <input id="author" type="text" name="author" />
+          <input
+            id="author"
+            type="text"
+            name="author"
+            value={author}
+            onChange={this.handleOnChange}
+          />
         </div>
-        <button disabled>Add Comment</button>
+        <button disabled={isDisabled}>Add Comment</button>
       </form>
     );
   }
--- src/components/CommentForm.test.jsx
+++ src/components/CommentForm.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 import CommentForm from "./CommentForm";
 
 describe("Comment Form", () => {
@@ -24,5 +24,31 @@ describe("Comment Form", () => {
       comment: ""
     });
     expect(submitButton).toBeDisabled();
+  });
+
+  test("when user fills in comment and name, the submit button is enabled", () => {
+    // Arrange
+    const comment = "Never put off until tomorrow what can be done today.";
+    const author = "Sensei Wu";
+
+    // Act
+    const { getByLabelText, getByPlaceholderText, getByText } = render(
+      <CommentForm />
+    );
+    const commentTextfieldNode = getByPlaceholderText("Write something...");
+    fireEvent.change(commentTextfieldNode, { target: { value: comment } });
+
+    const nameFieldNode = getByLabelText("Your Name");
+    fireEvent.change(nameFieldNode, { target: { value: author } });
+
+    // Assert
+    const form = document.querySelector('[data-testid="comment-form"]');
+    expect(form).toHaveFormValues({
+      author,
+      comment
+    });
+
+    const submitButton = getByText("Add Comment");
+    expect(submitButton).not.toBeDisabled();
   });
 });
