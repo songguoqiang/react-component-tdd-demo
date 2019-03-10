286649ff55591c5b813c0ef584625467a65cfe5a
gordon <gordon.song@thoughtworks.com>
2019-03-10 17:21:37 +0800

    install jest-dom in the project

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,5 @@
-import 'jest-prop-type-error'
+// throw error from console.error() when the message says there is prop type check error
+import "jest-prop-type-error";
+
+// add some helpful assertions
+import "jest-dom/extend-expect";
