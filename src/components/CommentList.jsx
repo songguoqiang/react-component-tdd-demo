ea6dc004e2f488bac7894a84890669d26aece30d
gordon <gordon.song@thoughtworks.com>
2019-03-10 15:56:11 +0800

    create CommentList component and add the first test

---
+++ src/components/CommentList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CommentCard from './CommentCard'
+
+const CommentList = ({ comments }) => (
+  <div>
+    {
+      comments.map(comment =>
+        <CommentCard key={comment.id} {...comment} />
+      )
+    }
+  </div>
+)
+
+export default CommentList;
