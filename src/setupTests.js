1fc8a3e3fcb81b221e9201b015fe608a075c5272
gordon <gordon.song@thoughtworks.com>
2019-03-10 17:38:33 +0800

    enable automatically clean up dom tree after each test case with react-testing-library

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@ import "jest-prop-type-error";
 
 // add some helpful assertions
 import "jest-dom/extend-expect";
+
+// this is basically: afterEach(cleanup)
+import "react-testing-library/cleanup-after-each";
